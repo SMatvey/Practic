abd87024c33de8607137d09b6cf013b0161fc3b6
Matvey <matveysvischo2004@gmail.com>
2023-01-16 04:49:50 +0200

    day_two and converter

---
+++ day_five/src/components/App.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="rule">
+    <input type="text" name="search" v-model="search"><br>
+    <table class="table table-dark">
+        <tr>
+        <th>ПІБ</th>
+        <th>Наявність ПР</th>
+        <th>Група</th>
+        <th>Оновлення</th>
+        <th>Видалення</th>
+      </tr>
+        <tr v-for="item in students"  v-bind:key="item._id"> 
+            <td>{{item.name}}</td>
+            <td><input type="checkbox" v-model="item.isDonePr"></td>
+            <td>{{item.group}}</td>
+            <td><a href = "#" @click="updateFind(item._id)">Оновити</a></td>
+            <td><a href = "#" @click="deleteStudent(item._id)">Видалити</a></td>  
+        </tr>
+    </table>
+
+    <input v-model="student.name">
+    <input type="checkbox" v-model="student.isDonePr">
+    <select v-model="student.group">
+        <option value="RPZ 19 1/9">RPZ 19 1/9</option>
+        <option value="RPZ 19 2/9">RPZ 19 2/9</option>
+    </select>
+    <button @click="addStudent()">Add student</button>
+    <button @click="updateStudent()">Update student</button>
+
+    <hr>
+    <section class="exchange">
+      <h2>Конвертор валюти</h2>
+      <article class="enterAmount">
+        <label for="amount">Введіть кількість:</label>
+        <input type="number" name="amount" v-model="amount" />
+      </article>
+      <article class="convert">
+        <label for="">Конвертувати з: </label>
+        <select name="from" id="" v-model="convertFrom">
+          <option value="UAH">UAH</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+        </select>
+        <label for="">Конвертувати в: </label>
+        <select name="to" id="" v-model="convertTo">
+          <option value="UAH">UAH</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+        </select>
+      </article>
+      <button @click="getCurrency">Зробити конвертацію</button>
+      <h3>Останні дані пишуть, що {{ rate }}</h3>
+    </section>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+ 
+export default {
+    data() {
+       return {
+            students: [],
+            updStudents: [],
+            search:'',
+            student: {name: "", isDonePr: false, group: ""},
+
+            convertFrom: "",
+            convertTo: "",
+            amount: "",
+            rate: ""
+       }
+    },
+    mounted: function () {
+        axios.get("http://34.82.81.113:3000/students")
+        .then(data => {
+            this.students =  data.data;
+            console.log(this.students)
+        })
+        
+    },
+    methods: {
+        addStudent() {
+            axios.post("http://34.82.81.113:3000/students", {
+            ...this.student,
+            })
+            .then((response) => {
+                this.students.push(response.data);
+            });
+        },
+        deleteStudent(studId) {
+            axios.delete(`http://34.82.81.113:3000/students/${studId}`)
+            .then((response) => {
+                this.students = this.students.filter((elem) => {
+                    return elem._id !== studId;
+                });
+            });
+        },
+        updateFind(studId) {
+            this.updStudents = {
+                ...this.students.find((elem) => elem._id == studId),
+            };
+            console.log(this.updStudents)
+            this.student._id=this.updStudents._id;
+            this.student.name=this.updStudents.name;
+            this.student.isDonePr=this.updStudents.isDonePr;
+            this.student.group=this.updStudents.group;
+        },
+        updateStudent() {
+            this.tempStudents = [];   
+            axios.put(`http://34.82.81.113:3000/students/${this.student._id}`, {
+            ...this.student,
+            })
+            .then((response) => {
+                this.students = response.data;
+                this.tempStudents = [];
+            });
+            getStudents()
+            axios.get("http://34.82.81.113:3000/students")
+                .then(data => {
+                    this.students =  data.data;
+                })   
+        },
+        getCurrency() {
+            axios
+            .get(
+                "https://api.apilayer.com/exchangerates_data/convert?to=" + this.convertTo +
+                "&from=" + this.convertFrom + "&amount=1",
+                {
+                headers: {
+                    apikey: "0sjTljESXiA3z56QG2VZ0P0Rjp7wGkfg",
+                },
+                }
+            )
+            .then((data) => {
+                console.log(data.data.info.rate);
+                this.rate = this.amount + " " + this.convertFrom + " equals " +
+                (data.data.info.rate * this.amount).toFixed(3) + " " + this.convertTo;
+            }); 
+        }
+    
+    }
+}
+
+function getStudents() {
+    return axios.get('http://34.82.81.113:3000/students')
+}
+
+</script>
--- day_five/webpack.config.js
+++ day_five/webpack.config.js
@@ -1,7 +1,41 @@
 const path = require('path');
 const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader')
  
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './main.js',
+    module: {
+        rules: [
+          // ... other rules
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+          },
+          {
+            test: /\.css$/,
+            oneOf: [
+                // this matches `<style module>`
+                {
+                    resourceQuery: /module/,
+                    use: [
+                        'vue-style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                },
+                // this matches plain `<style>` or `<style scoped>`
+                {
+                    use: ['vue-style-loader', 'css-loader']
+                }
+            ]
+        }
+        ]
+      },
    resolve: {
        alias: {
            vue: 'vue/dist/vue.esm-bundler'
@@ -16,6 +50,7 @@ module.exports = {
    new webpack.DefinePlugin({
      __VUE_OPTIONS_API__: true,
      __VUE_PROD_DEVTOOLS__: false
-   })
+   }),
+   new VueLoaderPlugin()
  ]
 };
